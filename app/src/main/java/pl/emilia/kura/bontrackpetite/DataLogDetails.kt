8080d10c328ca2d05dd16c8a1c89a34980ec1a42
Emilia Kura <emillia.kura@gmail.com>
2025-12-27 19:03:10 +0100

    Initial commit
    *Created app with 2 views. The 1st one contains a simple calendar widget. The 2nd one, for now, can only go back to 1st one and pop the view from the stack.
    *When user changes the date in the calendar the 2nd view appears.

---
+++ app/src/main/java/pl/emilia/kura/bontrackpetite/DataLogDetails.kt
@@ -0,0 +1,29 @@
+package pl.emilia.kura.bontrackpetite
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DataLogDetails : AppCompatActivity() {
+    lateinit var btnBack: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_data_log_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnBack=findViewById<Button>(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/pl/emilia/kura/bontrackpetite/MainActivity.kt
@@ -0,0 +1,35 @@
+package pl.emilia.kura.bontrackpetite
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var calendarView: CalendarView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        calendarView=findViewById<CalendarView>(R.id.calendarView)
+
+        calendarView.setOnDateChangeListener (
+            OnDateChangeListener { view, year, month, dayOfMonth ->
+                val date="$dayOfMonth - ${month+1} - $year"
+                val intent= Intent(this, DataLogDetails::class.java)
+                startActivity(intent)
+            }
+        )
+    }
+}
